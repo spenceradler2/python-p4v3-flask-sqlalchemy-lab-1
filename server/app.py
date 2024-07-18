dd4b400f0c63fe8df6aa793b002329b0414a6da0
Spencer <spencer.adler2@gmail.com>
2024-07-18 14:07:50 -0400

    completed

--- server/app.py
+++ server/app.py
@@ -21,7 +21,30 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def get_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id==id).first()
+    if earthquake:
+        body = earthquake.to_dict()
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+    return make_response(body, status)
 
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_magnitude_earthquake(magnitude):
+    earthquakes = []
+    for earthquake in Earthquake.query.filter(Earthquake.magnitude >= magnitude).all():
+        earthquakes.append(earthquake.to_dict())
+
+    body = {'count': len(earthquakes),
+        'quakes': earthquakes
+        }
+    status = 200
+
+    return make_response(body, status)
+    
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
